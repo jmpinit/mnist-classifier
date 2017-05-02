3ab7faf785060d255b0509d9126bbfc9e3b52dd2
Owen Trueblood <jmptable@mit.edu>
2017-05-01 22:21:49 -0400

    Script to download MNIST data sets

---
+++ getdata.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DATA_DIR=$SCRIPT_DIR/data
+
+TRAINING_SET_IMAGES=http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+TRAINING_SET_LABELS=http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+TEST_SET_IMAGES=http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+TEST_SET_LABELS=http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+
+curl $TRAINING_SET_IMAGES | gzip -d > $DATA_DIR/train-images-idx3-ubyte
+curl $TRAINING_SET_LABELS | gzip -d > $DATA_DIR/train-labels-idx1-ubyte
+curl $TEST_SET_IMAGES | gzip -d > $DATA_DIR/t10k-images-idx3-ubyte
+curl $TEST_SET_LABELS | gzip -d > $DATA_DIR/t10k-labels-idx1-ubyte
+
