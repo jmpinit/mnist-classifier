8a98ec66a9e715bc62f8dfb5477c943904109204
Owen Trueblood <jmptable@mit.edu>
2017-05-02 00:48:48 -0400

    1st test image -> a PNG file

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,31 @@
 module Main where
 
-import Codec.Picture.Types as PicTypes
-import Codec.Picture.Png as Png
+import qualified Data.ByteString.Lazy as BS
+import qualified Codec.Picture.Types as PicTypes
+import qualified Codec.Picture.Png as Png
+import Data.IDX
+import Data.List.Split
+import Data.Vector.Unboxed hiding (map, head)
+
+import Data.Maybe -- FIXME: for fromJust, which can throw an error
 
 genFooImage x y = PicTypes.PixelRGB8 (fromIntegral x) (fromIntegral y) 128
 
 fooImage = PicTypes.generateImage genFooImage 250 300
 
+matrixToPNG :: Int -> Int -> [Int] -> PicTypes.Image PicTypes.PixelRGB8
+matrixToPNG w h matrix = (PicTypes.generateImage rasterize w h) where
+  gray = \b -> PicTypes.PixelRGB8 b b b
+  rasterize = \x y -> gray (fromIntegral (matrix !! (y * w + x)))
+
+idxToPng :: IDXData -> [PicTypes.Image PicTypes.PixelRGB8]
+idxToPng idxData = map (matrixToPNG width height) imageMatrices where
+  [numImages, width, height] = toList (idxDimensions idxData)
+  imageData = idxIntContent idxData
+  imageMatrices = chunksOf (width * height) (toList imageData)
+
 main :: IO ()
 main = do
-  writePng "./test.png" fooImage
-  putStrLn "hello world"
+  imagesInIDX <- decodeIDXFile "./data/t10k-images-idx3-ubyte"
+  let firstImage = head $ idxToPng (fromJust imagesInIDX) in
+    Png.writePng "./first.png" firstImage
