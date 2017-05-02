c812e8fd88ace45c45ce929ad912aa2e7062bf06
Owen Trueblood <jmptable@mit.edu>
2017-05-02 10:53:06 -0400

    Style changes suggested by hlint

--- src/Main.hs
+++ src/Main.hs
@@ -14,9 +14,9 @@ genFooImage x y = PicTypes.PixelRGB8 (fromIntegral x) (fromIntegral y) 128
 fooImage = PicTypes.generateImage genFooImage 250 300
 
 matrixToPNG :: Int -> Int -> [Int] -> PicTypes.Image PicTypes.PixelRGB8
-matrixToPNG w h matrix = (PicTypes.generateImage rasterize w h) where
-  gray = \b -> PicTypes.PixelRGB8 b b b
-  rasterize = \x y -> gray (fromIntegral (matrix !! (y * w + x)))
+matrixToPNG w h matrix = PicTypes.generateImage rasterize w h where
+  gray b = PicTypes.PixelRGB8 b b b
+  rasterize x y = gray (fromIntegral (matrix !! (y * w + x)))
 
 idxToPng :: IDXData -> [PicTypes.Image PicTypes.PixelRGB8]
 idxToPng idxData = map (matrixToPNG width height) imageMatrices where
