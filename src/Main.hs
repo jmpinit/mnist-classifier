4f2fd291b7f7885bddf67b11bdc5b442270af61b
Owen Trueblood <jmptable@mit.edu>
2017-05-01 22:53:18 -0400

    We can save images to files!

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import Codec.Picture.Types as PicTypes
+import Codec.Picture.Png as Png
+
+genFooImage x y = PicTypes.PixelRGB8 (fromIntegral x) (fromIntegral y) 128
+
+fooImage = PicTypes.generateImage genFooImage 250 300
+
 main :: IO ()
 main = do
+  writePng "./test.png" fooImage
   putStrLn "hello world"
